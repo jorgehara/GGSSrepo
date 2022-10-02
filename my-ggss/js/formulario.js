bd574a26d57d0def68da05203098d7c6814ebe86
Jorge <88298206+jorgehara@users.noreply.github.com>
2022-10-02 15:47:45 -0300

    pushUpdateInputlook&Feel

--- my-ggss/js/formulario.js
+++ my-ggss/js/formulario.js
@@ -6,7 +6,6 @@ const expresiones = {
 	// nombre: /^[a-zA-ZÀ-ÿ\s]{1,40}$/, // Letras y espacios, pueden llevar acentos.
 	// nombre: /^[a-zA-ZÀ-ÿ\s]$/, // Letras y espacios, pueden llevar acentos.
 	nombre: /^[A-Za-z]+$/, // Letras y espacios, pueden llevar acentos.
-	
 	password: /^.{4,12}$/, // 4 a 12 digitos.
 	correo: /^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$/,
 	telefono: /^\d{7,14}$/ // 7 a 14 numeros.
