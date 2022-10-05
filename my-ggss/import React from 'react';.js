0a0a86da671a6ee96f593391807b52b09445734c
Jorge <88298206+jorgehara@users.noreply.github.com>
2022-10-05 10:38:51 -0300

    PushTraerPull

---
+++ my-ggss/import React from 'react';.js
@@ -0,0 +1,36 @@
+import React from 'react';
+import axios from 'axios';
+
+const FormOne = () => {
+  // a local state to store the currently selected file.
+  const [selectedFile, setSelectedFile] = React.useState(null);
+
+  const handleSubmit = async(event) => {
+    event.preventDefault()
+    const formData = new FormData();
+    formData.append("selectedFile", selectedFile);
+    try {
+      const response = await axios({
+        method: "post",
+        url: "/api/upload/file",
+        data: formData,
+        headers: { "Content-Type": "multipart/form-data" },
+      });
+    } catch(error) {
+      console.log(error)
+    }
+  }
+
+  const handleFileSelect = (event) => {
+    setSelectedFile(event.target.files[0])
+  }
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <input type="file" onChange={handleFileSelect}/>
+      <input type="submit" value="Upload File" />
+    </form>
+  )
+};
+
+export default FormOne;
