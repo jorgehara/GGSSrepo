8b5de85e7adc317f6f75d7bbf355c5cfa114d5a0
jorgehara <empresamodelo2018@gmail.com>
2022-10-12 16:43:19 -0300

    pushUpdateMakeMigrationToReact

--- my-ggss/src/index.js
+++ my-ggss/src/index.js
@@ -3,11 +3,13 @@ import ReactDOM from 'react-dom';
 //import { createRoot } from 'react-dom/client'; para versions of React after 18
 import './index.css';
 import Formulario from './Formulario';
+import DragArea from './DragDrop';
 
 ReactDOM.render(
 	<React.StrictMode>
 		<div className="contenedor">
 			<Formulario />
+			<DragArea/>
 		</div>
 	</React.StrictMode>,
 	document.getElementById('root')
