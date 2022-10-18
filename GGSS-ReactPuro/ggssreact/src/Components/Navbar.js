9dc3eed95b7fb79b3338e50b9121c0fe13de449e
lautarxtomas <lautaro.martinez88@mi.unc.edu.ar>
2022-10-18 16:05:37 -0300

    commit_2

--- GGSS-ReactPuro/ggssreact/src/Components/Navbar.js
+++ GGSS-ReactPuro/ggssreact/src/Components/Navbar.js
@@ -16,7 +16,7 @@ const Navbar = () => {
 			<div class="collapse navbar-collapse" id="navbarNav">
 			<ul class="navbar-nav">
 				<li class="nav-item">
-				<a class="nav-link active" aria-current="page" href="/some/valid/uri">Personalessssss</a>
+				<a class="nav-link active" aria-current="page" href="/some/valid/uri">Personales</a>
 				</li>
 				<li class="nav-item">
 				<a class="nav-link" href="/some/valid/uri">Familia</a>
