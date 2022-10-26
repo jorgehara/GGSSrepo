0ea60b5e3a48bace53c540a25d7fdfc47724ebad
jorgehara <empresamodelo2018@gmail.com>
2022-10-26 17:45:22 -0300

    pushUpdate

--- GGSS-ReactPuro/ggssreact/src/components/DatosPersonales/DatosPersonales.jsx
+++ GGSS-ReactPuro/ggssreact/src/components/DatosPersonales/DatosPersonales.jsx
@@ -1,4 +1,4 @@
-import React, { useContext, useEffect, useState } from "react";
+import React, { useContext, useEffect} from "react";
 // import swal from "sweetalert";
 import DNICboBox from "../Inputs/DNICboBox/DNICboBox";
 import InputButton from "../Inputs/InputButton/InputButton";
