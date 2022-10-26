cfc9b2ed0c2d6b73d7168502a15d54f4f3fac7fc
jorgehara <empresamodelo2018@gmail.com>
2022-10-26 16:23:06 -0300

    pushUpate-FromTesting

--- GGSS-ReactPuro/ggssreact/src/components/Modals/BasicModalObs/BasicModalObs.jsx
+++ GGSS-ReactPuro/ggssreact/src/components/Modals/BasicModalObs/BasicModalObs.jsx
@@ -65,32 +65,4 @@ const BasicModalObs = ({ idModal, nameModal, nameOptionModal, array }) => {
         </div>
     )
 }
-
-            <div className="modal-footer">
-              <div className="crudBtns">
-                <button type="button" className="btn btn-success crudBtn">
-                  AGREGAR
-                </button>
-                <button type="button" className="btn btn-warning crudBtn">
-                  MODIFICAR
-                </button>
-                <button type="button" className="btn btn-danger crudBtn">
-                  ELIMINAR
-                </button>
-              </div>
-              <button
-                type="button"
-                className="btn btn-secondary"
-                data-bs-dismiss="modal"
-              >
-                SALIR
-              </button>
-            </div>
-          </div>
-        </div>
-      </div>
-    </div>
-  );
-};
-
 export default BasicModalObs;
