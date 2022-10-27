5d8b0b00d4e4de5c9d3e9e79afe6f39fcfa19704
jorgehara <empresamodelo2018@gmail.com>
2022-10-27 12:55:18 -0300

    pushUpdateReviewComponentes

--- GGSS-ReactPuro/ggssreact/src/components/Modals/BasicModal/BasicModal.jsx
+++ GGSS-ReactPuro/ggssreact/src/components/Modals/BasicModal/BasicModal.jsx
@@ -1,42 +1,40 @@
 import React from 'react'
-import InputCbo from '../../Inputs/InputCbo/InputCbo';
-import './BasicModal.css'
-import '../Modales.css'
+import TextArea from '../../Inputs/TextArea/TextArea';
 
-const BasicModal = ({ idModal, nameModal, nameOptionModal, array }) => {
-
-    console.log(nameOptionModal)
-    return (
-        <div>
+const BasicModal = ({idModal, nameModal, nameOptionModal,array}) => {
+  return (
+    <div>
             <div className="modal fade" id={idModal} tabindex="-1" aria-labelledby={`${idModal}Label`} aria-hidden="true">
-                <div className="modal-dialog modal-xl">
+                <div className="modal-dialog">
                     <div className="modal-content">
                         <div className="modal-header">
                             <h1 className="modal-title fs-5" id={`${idModal}Label`}>
                                 {nameModal}
                             </h1>
-                            <button type="button" className="btn-close btn-close-white" data-bs-dismiss="modal" aria-label="Close"></button>
+                            <button type="button" className="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
                         </div>
                         <div className="modal-body">
                             <div className="llamadaApi">
-
-                                <label htmlFor="data">Datos: </label>
+                                {
+                                    
+                                }
+                            </div>
+                            <div className="bodyInputs">
+                                <label htmlFor="calle" style={{ marginRight: "15px" }}> {nameOptionModal}: </label>
+                                <input type="text" name="calle" />
+                                <br />
+                                {/* ESTE TIENE QUE SER UN SELECTOR  */}
+                                <label htmlFor="obs">Observaciones: </label>
                                 <br />
                                 <select class="form-select row mt-1" multiple aria-label="multiple select example">
                                     <option value="1">One</option>
                                     <option value="2">Two</option>
                                     <option value="3">Three</option>
                                 </select>
-
-                            </div>
-                            <div className="bodyInputs">
-                                <label htmlFor={idModal} style={{ marginRight: "15px" }}> {nameOptionModal}: </label>
-                                <input type="text" name={idModal} />
-                               
+                                <TextArea inputName="Observaciones" />
                                 <hr />
-
                                 <div className="btnInputs">
-                                    <button type="button" className="btn btn-danger btnAceptar">
+                                    <button type="button" className="btn btn-success btnAceptar">
                                         ACEPTAR
                                     </button>
                                     <button type="button" className="btn btn-danger">
@@ -48,10 +46,10 @@ const BasicModal = ({ idModal, nameModal, nameOptionModal, array }) => {
 
                         <div className="modal-footer">
                             <div className="crudBtns">
-                                <button type="button" className="btn btn-danger crudBtn">
+                                <button type="button" className="btn btn-success crudBtn">
                                     AGREGAR
                                 </button>
-                                <button type="button" className="btn btn-danger crudBtn">
+                                <button type="button" className="btn btn-warning crudBtn">
                                     MODIFICAR
                                 </button>
                                 <button type="button" className="btn btn-danger crudBtn">
@@ -65,8 +63,8 @@ const BasicModal = ({ idModal, nameModal, nameOptionModal, array }) => {
                     </div>
                 </div>
             </div>
-        </div>
-    )
+    </div>
+  )
 }
 
 export default BasicModal;
