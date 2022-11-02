c266c4f47e731bce672bf927752f6a11ed2b9f7e
jorgehara <empresamodelo2018@gmail.com>
2022-11-02 11:45:14 -0300

    pushUpdate-errorModal

--- GGSS-ReactPuro/ggssreact/src/components/Modals/BasicModal/BasicModal.jsx
+++ GGSS-ReactPuro/ggssreact/src/components/Modals/BasicModal/BasicModal.jsx
@@ -4,13 +4,12 @@ import "./BasicModal.css";
 import '../Modales.css'
 import InputModal from '../../Inputs/InputModal/InputModal';
 
-const BasicModal = ({ idModal, nameModal, nameOptionModal, array, textArea, placeholder }) => {
+const BasicModal = ({ idModal, nameModal, array, textArea, placeholder }) => {
 
-    console.log(nameOptionModal)
     return (
         <div>
             <div className="modal fade" id={idModal} tabindex="-1" aria-labelledby={`${idModal}Label`} aria-hidden="true">
-                <div className="modal-dialog modal-xl">
+                <div className="modal-dialog modal-dialog-centered modal-xl">
                     <div className="modal-content">
                         <div className="modal-header">
                             <h1 className="modal-title fs-5" id={`${idModal}Label`}>
@@ -20,7 +19,6 @@ const BasicModal = ({ idModal, nameModal, nameOptionModal, array, textArea, plac
                         </div>
                         <div className="modal-body">
                             <div className="llamadaApi">
-
                                 <label htmlFor="data">Datos: </label>
                                 <br />
                                 <select class="form-select row mt-1" multiple aria-label="multiple select example">
@@ -28,12 +26,8 @@ const BasicModal = ({ idModal, nameModal, nameOptionModal, array, textArea, plac
                                     <option value="2">Two</option>
                                     <option value="3">Three</option>
                                 </select>
-
                             </div>
                             <div className="bodyInputs">
-                                {/* <label htmlFor={idModal} style={{ marginRight: "15px" }}> {nameOptionModal}: </label>
-                                <input type="text" name={idModal} /> */}
-
                                 {
                                     placeholder.map((p, i) => {
                                         return(
@@ -41,14 +35,9 @@ const BasicModal = ({ idModal, nameModal, nameOptionModal, array, textArea, plac
                                         )
                                     })
                                 }
-
-                        
                                 <br />
-
                                 {textArea && <TextArea inputName="Observaciones" /> }
-                                
                                 <hr />
-
                                 <div className="btnInputs">
                                     <button type="button" className="btn btn-danger btnAceptar">
                                         ACEPTAR
