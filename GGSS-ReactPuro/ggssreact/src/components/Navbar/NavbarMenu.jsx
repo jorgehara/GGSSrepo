f18553fa055e90c00cbad73094c5c08c5e030e56
Rodrigo <rodrigo@inft.com.ar>
2022-11-04 12:28:08 -0300

    Fix Navbar

--- GGSS-ReactPuro/ggssreact/src/components/Navbar/NavbarMenu.jsx
+++ GGSS-ReactPuro/ggssreact/src/components/Navbar/NavbarMenu.jsx
@@ -54,7 +54,7 @@ const parentesco = saveParen !== undefined ? saveParen.map((par,i)=> {return(par
                     <a className="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
                         Empleados
                     </a>
-                    <ul className="dropdown-menu">
+                    <ul className="dropdown-menu">		
                         <li><Link className="dropdown-item" to="/home">Ficha Empleados</Link></li>
                         <li><Link className="dropdown-item" to="#">Busqueda de Datos</Link></li>
                     </ul>
@@ -73,50 +73,57 @@ const parentesco = saveParen !== undefined ? saveParen.map((par,i)=> {return(par
                         Tabla de Datos
                     </a>
                     <ul className="dropdown-menu">
-						<h6 className="titleEmpleados">Para empleados: </h6>
-						<div className="datosEmpleados">
-							<ButtonCallModal idModal="EstadoCivil" nameButton="Estados Civiles" useNavbar={true} />
-							<ButtonCallModal idModal="Estudios" nameButton="Estudios" useNavbar={true} />
-							<ButtonCallModal idModal="TipoDocumento" nameButton="Tipo de Documento" useNavbar={true} />
-							<ButtonCallModal idModal="Parentescos" nameButton="Parentescos" useNavbar={true} />
-							<ButtonCallModal idModal="estadosEmpleados" nameButton="Estados para empleados" useNavbar={true} />
-							<ButtonCallModal idModal="cargos" nameButton="Cargos" useNavbar={true} />
-							<ButtonCallModal idModal="tareasDesempeñadas" nameButton="Tareas Desempeñadas" useNavbar={true} />
-							<ButtonCallModal idModal="formasDePago" nameButton="Formas de Pago" useNavbar={true} />
-							<ButtonCallModal idModal="modosDeContratacion" nameButton="Modos de Contratación" useNavbar={true} />
-							<ButtonCallModal idModal="modosDeLiquidacion" nameButton="Modos de Liquidacion" useNavbar={true} />
-							<ButtonCallModal idModal="motivosEgreso" nameButton="Motivos de Egreso" useNavbar={true}/>
-							<ButtonCallModal idModal="paises" nameButton="Paises" useNavbar={true} />
-							<ButtonCallModal idModal="pdlb" nameButton="Provincias - Departamentos - Localidades - Barrios" useNavbar={true} />
-							<ButtonCallModal idModal="calles" nameButton="Calles" useNavbar={true} />
-							<ButtonCallModal idModal="empleadores" nameButton="Empleadores" useNavbar={true} />
-							<ButtonCallModal idModal="alicuotas" nameButton="Alicuotas" useNavbar={true} />
-							
-						</div>
+						<li class="dropdown-submenu">
+						<a className='dropdown-item' tabindex="-1" href="#">Para Empleados</a>
+							<ul class="dropdown-menu">
+								<div className="datosEmpleados">
+									<ButtonCallModal idModal="EstadoCivil" nameButton="Estados Civiles" useNavbar={true} />
+									<ButtonCallModal idModal="Estudios" nameButton="Estudios" useNavbar={true} />
+									<ButtonCallModal idModal="TipoDocumento" nameButton="Tipo de Documento" useNavbar={true} />
+									<ButtonCallModal idModal="Parentescos" nameButton="Parentescos" useNavbar={true} />
+									<ButtonCallModal idModal="estadosEmpleados" nameButton="Estados para empleados" useNavbar={true} />
+									<ButtonCallModal idModal="cargos" nameButton="Cargos" useNavbar={true} />
+									<ButtonCallModal idModal="tareasDesempeñadas" nameButton="Tareas Desempeñadas" useNavbar={true} />
+									<ButtonCallModal idModal="formasDePago" nameButton="Formas de Pago" useNavbar={true} />
+									<ButtonCallModal idModal="modosDeContratacion" nameButton="Modos de Contratación" useNavbar={true} />
+									<ButtonCallModal idModal="modosDeLiquidacion" nameButton="Modos de Liquidacion" useNavbar={true} />
+									<ButtonCallModal idModal="motivosEgreso" nameButton="Motivos de Egreso" useNavbar={true}/>
+									<ButtonCallModal idModal="paises" nameButton="Paises" useNavbar={true} />
+									<ButtonCallModal idModal="pdlb" nameButton="Provincias - Departamentos - Localidades - Barrios" useNavbar={true} />
+									<ButtonCallModal idModal="calles" nameButton="Calles" useNavbar={true} />
+									<ButtonCallModal idModal="empleadores" nameButton="Empleadores" useNavbar={true} />
+									<ButtonCallModal idModal="alicuotas" nameButton="Alicuotas" useNavbar={true} />								
+								</div>
+							</ul>
+						</li>						
+						
 						{/* {/ <button onClick={handleDatosEmpleados}>Ocultar lista</button> /} */}
 						<hr />
-						<h6 className="titleLiquidacion">Para liquidación: </h6>
-						<div className="datosLiquidacion">
-							<ButtonCallModal idModal="Bancos" nameButton="Bancos" useNavbar={true} />
-							<ButtonCallModal idModal="Telefonia" nameButton="Empresas de telefonia celular" useNavbar={true} />
-							<ButtonCallModal idModal="Sindicatos" nameButton="Sindicatos" useNavbar={true} />
-							<ButtonCallModal idModal="Obras Sociales" nameButton="Obras Sociales" useNavbar={true} />
-							<ButtonCallModal idModal="AFJP" nameButton="A.F.J.P" useNavbar={true} />
-							<ButtonCallModal idModal="Centros de Costo" nameButton="Centros de Costo" useNavbar={true} />
-							<ButtonCallModal idModal="Sectores/Departamentos" nameButton="Sectores/Departamentos" useNavbar={true} />
-							<ButtonCallModal idModal="Direcciones" nameButton="Direcciones" useNavbar={true} />
-							<ButtonCallModal idModal="Lugares de Pago" nameButton="Lugares de Pago" useNavbar={true} />
-							<ButtonCallModal idModal="Documentacion" nameButton="Documentación que presentan los empleados" useNavbar={true} />
-							<ButtonCallModal idModal="Reducción" nameButton="Reducción de Deducciones de Ganancias" useNavbar={true} />
-							<ButtonCallModal idModal="Escala" nameButton="Escala de Ganancias" useNavbar={true} />
-							<ButtonCallModal idModal="Deducciones" nameButton="Deducciones de Ganancias" useNavbar={true} />
-							<ButtonCallModal idModal="Valores" nameButton="Valores Permanencia Categoría" useNavbar={true} />
-							<ButtonCallModal idModal="Convenios" nameButton="Convenios, Categorías, Básicos y Antigüedad" useNavbar={true} />
-							<ButtonCallModal idModal="Jerarquía" nameButton="Jerarquía de Categorías" useNavbar={true} />
-							<ButtonCallModal idModal="Licencias" nameButton="Licencias por Antigüedad" useNavbar={true} />
-							<ButtonCallModal idModal="Plan" nameButton="Plan de Cuentas" useNavbar={true} />
-						</div>
-
+						<li class="dropdown-submenu">
+						<a className='dropdown-item' tabindex="-1" href="#">Para Liquidación</a>
+							<ul class="dropdown-menu">
+								<div className="datosLiquidacion">
+									<ButtonCallModal idModal="Bancos" nameButton="Bancos" useNavbar={true} />
+									<ButtonCallModal idModal="Telefonia" nameButton="Empresas de telefonia celular" useNavbar={true} />
+									<ButtonCallModal idModal="Sindicatos" nameButton="Sindicatos" useNavbar={true} />
+									<ButtonCallModal idModal="Obras Sociales" nameButton="Obras Sociales" useNavbar={true} />
+									<ButtonCallModal idModal="AFJP" nameButton="A.F.J.P" useNavbar={true} />
+									<ButtonCallModal idModal="Centros de Costo" nameButton="Centros de Costo" useNavbar={true} />
+									<ButtonCallModal idModal="Sectores/Departamentos" nameButton="Sectores/Departamentos" useNavbar={true} />
+									<ButtonCallModal idModal="Direcciones" nameButton="Direcciones" useNavbar={true} />
+									<ButtonCallModal idModal="Lugares de Pago" nameButton="Lugares de Pago" useNavbar={true} />
+									<ButtonCallModal idModal="Documentacion" nameButton="Documentación que presentan los empleados" useNavbar={true} />
+									<ButtonCallModal idModal="Reducción" nameButton="Reducción de Deducciones de Ganancias" useNavbar={true} />
+									<ButtonCallModal idModal="Escala" nameButton="Escala de Ganancias" useNavbar={true} />
+									<ButtonCallModal idModal="Deducciones" nameButton="Deducciones de Ganancias" useNavbar={true} />
+									<ButtonCallModal idModal="Valores" nameButton="Valores Permanencia Categoría" useNavbar={true} />
+									<ButtonCallModal idModal="Convenios" nameButton="Convenios, Categorías, Básicos y Antigüedad" useNavbar={true} />
+									<ButtonCallModal idModal="Jerarquía" nameButton="Jerarquía de Categorías" useNavbar={true} />
+									<ButtonCallModal idModal="Licencias" nameButton="Licencias por Antigüedad" useNavbar={true} />
+									<ButtonCallModal idModal="Plan" nameButton="Plan de Cuentas" useNavbar={true} />
+								</div>
+							</ul>
+						</li>								
                     </ul>
                 </li>
 				<li className="nav-item">
