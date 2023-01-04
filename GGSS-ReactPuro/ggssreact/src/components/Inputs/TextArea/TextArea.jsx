d78d829bcae29a91c8d967fd85959b66aabf6a79
jorgehara <empresamodelo2018@gmail.com>
2023-01-04 13:08:25 -0300

    pushUpdateTextAreacon Contador Perfecto

--- GGSS-ReactPuro/ggssreact/src/components/Inputs/TextArea/TextArea.jsx
+++ GGSS-ReactPuro/ggssreact/src/components/Inputs/TextArea/TextArea.jsx
@@ -69,47 +69,54 @@
 
 // export default TextArea
 
-import React from 'react'
+import React, { useState } from 'react'
 import './TextArea.css'
 
 
-const TextArea = () => {
-    (function(){
-        document.addEventListener("keyup", function(event){
-            if(event.target.matches(".count-chars")){
-                // get input value and length
-                const value = event.target.value;
-                const valueLength = event.target.value.length;
-                // get data value
-                const maxChars = parseInt(event.target.getAttribute("data-max-chars"));
-                const remainingChars = maxChars - valueLength;
-                if(valueLength > maxChars){
-                    // limit chars to maxChars
-                    // event.target.value = value.substr(0, maxChars)
-                    return;  //end function execution
-                }
-                event.target.nextElementSibling.innerHTML = remainingChars + " characters remaining";
-            }
-        })
-    })();
+const TextArea = ({ characterLimit = 255 }) => {
+    const [input, setInput] = useState('');
+
+    function handleChange(event) {
+        const newInput = event.target.value;
+        setInput(newInput);
+      }
+    
+      const charactersLeft = characterLimit - input.length;
+
+   
 return (
     <>
-        <div class="count-container">
-        <form>
-            <div class="input-group">
-            </div>
-            <div className='form__grupo__label pl-1 mt-3'>
-         <label className='form__grupo__label__label m-0' htmlFor="legajo">Observaciones</label>
-            <div class="input-group">
-            <input placeholder="Ingrese Observaciones" maxlength="255" data-max-chars="255" class=" formulario-input-TextArea form-control-obs txtArea input-control count-chars">
-                
-            </input>
-        <div class="input-msg text-black"></div>
-            </div>
-            </div>
 
+<div class="count-container">
+        <form>
+          <div class="input-group">
+            <div className="form__grupo__label pl-1 mt-3">
+              <label
+                className="form__grupo__label__label m-0"
+                htmlFor="legajo"
+              >
+                Observaciones
+              </label>
+              <div class="input-group">
+                <input
+                  placeholder="Ingrese Observaciones"
+                  maxLength={characterLimit}
+                  class="formulario-input-TextArea form-control-obs txtArea input-control count-chars"
+                  onChange={handleChange}
+                  value={input}
+                />
+                <div class="input-msg text-black">
+                  Character Count: {input.length}
+                  Characters left: {charactersLeft}
+                  </div>
+                {charactersLeft <= 255 && (
+                  <p className="alert">No more characters left!</p>
+                )}
+              </div>
+            </div>
+          </div>
         </form>
-        </div>
+      </div>
     </>
 )
 }
